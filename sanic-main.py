abb93c3a89953e1ff0877349ac6981646131af8e
shay margolis <shay@inbar.co.il>
2023-08-13 16:30:20 +0300

    sanic-main: Fix ASGI run

--- sanic-main.py
+++ sanic-main.py
@@ -8,12 +8,10 @@ from visual_lab import Base
 from options import g_options
 from fetch_data import fetch_data_periodic
 
-app = Sanic("MyApp")
-configure(app, Base, options=g_options)
-app.static("/static", "./static")
-app.add_task(fetch_data_periodic())
+def main():
+    app = Sanic("MyApp")
+    configure(app, Base, options=g_options)
+    app.static("/static", "./static")
+    app.add_task(fetch_data_periodic())
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0")  # Run the ASGI app
-
-
+    return app
