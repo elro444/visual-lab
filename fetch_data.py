d840531e5b8a8919834d4230bdb0470c50ccb3ee
shay margolis <shay@inbar.co.il>
2023-08-13 16:45:22 +0300

    fix spacing

--- fetch_data.py
+++ fetch_data.py
@@ -2,14 +2,14 @@ import time
 import asyncio
 
 def real_fetch_data_single():
-	print("Starting to fetch...")
-	# "Busy" IO
-	time.sleep(5)
-	print("Fetch finished!")
+    print("Starting to fetch...")
+    # "Busy" IO
+    time.sleep(5)
+    print("Fetch finished!")
 
 async def fetch_data_periodic():
-	loop = asyncio.get_event_loop()
+    loop = asyncio.get_event_loop()
 
-	while True:
-		await loop.run_in_executor(None, real_fetch_data_single)
-		await asyncio.sleep(5)
+    while True:
+        await loop.run_in_executor(None, real_fetch_data_single)
+        await asyncio.sleep(5)
