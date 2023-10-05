be686ec600ed5a12e870a21a2661e01a7f939a9c
Elroi Allouch <elro444@gmail.com>
2023-10-05 17:03:39 +0300

    visual_lab: Add newline at EOF

---
+++ consts.py
@@ -0,0 +1,13 @@
+STATUSES = \
+    ['good'] * 20 + \
+    ['down'] * 2 + \
+    ['unknown-device'] * 5 + \
+    ['broken', 'misconfigured']
+
+COLORS = {
+    'good': '#72fa93',
+    'down': '#e45f2b',
+    'unknown-device': '#f6c445',
+    'broken': '#e39af0',
+    'misconfigured': '#9ac1f0',
+}
--- visual_lab.py
+++ visual_lab.py
@@ -16,4 +16,3 @@ def Base():
             *[Cabinet(x) for x in 'ABCDEFGH']
         ),
     )
-
